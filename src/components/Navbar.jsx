8bcaa58cd17197f77ca04a86df064bdc496b7d26
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-02-24 11:40:33 +0100

    feat:Added the Navbar & Herosection components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const [button, setbutton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setbutton(false);
+    } else {
+      setbutton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                SERVICES
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                PRODUCTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                SIGN UP
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn-outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import "../../App.css";
+import HeroSection from "../HeroSection";
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+export default Home;
