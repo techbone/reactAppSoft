61cbe64678e15995b5dd699cc1d5784af4b058f0
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-02-28 23:58:43 +0100

    feat:service page prototype

--- src/components/pages/services.js
+++ src/components/pages/services.js
@@ -16,6 +16,11 @@ const Services = () => {
     <>
       <div className="services-container">
         <h1 className="welcome">WELCOME TO ETU</h1>
+        <p>
+          Get started on your vacation planning by exploring Australia via our
+          360° videos
+        </p>
+        <button className="degree">watch 360° videos</button>
       </div>
     </>
   );
