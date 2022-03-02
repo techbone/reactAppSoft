332683021682f19eefb45b64b3ebfd6fbb1fb3c9
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-03-02 23:02:38 +0100

    feat:service page prototype step 2

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,7 +1,50 @@
+// import React from "react";
+
+// import "../../App.css";
+
+// export default function Products() {
+//   return <h1 className="products">PRODUCTS</h1>;
+// }
+
 import React from "react";
+import "../Products.css";
+import imageOne from "../Images/img-home.jpeg";
+import imageTwo from "../Images/img-9.jpeg";
+import imageThree from "../Images/img-2.jpeg";
 
-import "../../App.css";
+const Products = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="brazil">Enjoy wonderful experiences in Brazil</h1>
+        <div className="grids">
+          <div className="grid">
+            <img src={imageOne} alt="imageOne" />
+            <h2 className="rio">
+              Rio de janeiro
+              <br />
+              Beaches
+            </h2>
+            <p className="brazil-text">
+              Nothing is more Brazilian than the beaches of Rio.Urbanized and
+              preserved to the right extent,They represent perfectly the
+              cariocas and their joy.And i said Nothing is more Brazilian than
+              the beaches of Rio.Urbanized and preserved to the right
+              extent,They represent perfectly the cariocas and their joy.
+            </p>
+            <hr />
+            <p>Durat</p>
+          </div>
+          <div className="grid">
+            <img src={imageTwo} alt="imageOne" />
+          </div>
+          <div className="grid">
+            <img src={imageThree} alt="imageOne" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default function Products() {
-  return <h1 className="products">PRODUCTS</h1>;
-}
+export default Products;
--- src/components/pages/services.js
+++ src/components/pages/services.js
@@ -7,6 +7,7 @@
 // }
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../../App.css";
 import "../service.css";
@@ -15,12 +16,14 @@ const Services = () => {
   return (
     <>
       <div className="services-container">
-        <h1 className="welcome">WELCOME TO ETU</h1>
+        <h1 className="welcome">WELCOME TO ETUDAYE</h1>
         <p>
           Get started on your vacation planning by exploring Australia via our
           360° videos
         </p>
-        <button className="degree">watch 360° videos</button>
+        <Link to="/">
+          <button className="degree">watch 360° videos</button>
+        </Link>
       </div>
     </>
   );
